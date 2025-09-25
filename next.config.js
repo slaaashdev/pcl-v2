45538256ff0f6600cd22d1c37a990e6abdcff4b0
PCL Developer <slaaashdev@github.com>
2025-09-25 09:58:17 +0800

    Fix Next.js build stack overflow by disabling outputFileTracing

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/@swc/core-linux-x64-gnu',
+        'node_modules/@swc/core-linux-x64-musl',
+        'node_modules/@esbuild/linux-x64',
+      ],
+    },
+  },
+  outputFileTracing: false, // Disable file tracing to prevent stack overflow
 }
 
 module.exports = nextConfig
